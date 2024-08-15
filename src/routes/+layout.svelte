8d03ff4f1ea75ce1cfb2c8e1d9335e3201d66f14
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-08-15 19:43:05 +0200

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	const favicon = '/favicon.svg';
+	const { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Renew</title>
+</svelte:head>
+
+<h2 class="app-name"><img src={favicon} alt="Renew" class="icon" /> Renew</h2>
+
+{@render children()}
+
+<style>
+	.icon {
+		width: 1.5em;
+		height: 1.5em;
+	}
+
+	.app-name {
+		display: flex;
+		align-items: center;
+		gap: 0.5ex;
+		padding: 0.5ex;
+		margin: 0;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+		font-family: monospace;
+	}
+</style>
