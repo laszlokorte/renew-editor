80566142ee32ef6cf28a3851ca8aa41ce39a283f
Laszlo Korte <me@laszlokorte.de>
2024-11-30 15:44:12 +0100

    show group bounding box

--- src/routes/documents/[document_id]/editor/+page.svelte
+++ src/routes/documents/[document_id]/editor/+page.svelte
@@ -209,13 +209,67 @@
 		return doc.layers.items
 			.map((l, index) => ({ l, index }))
 			.filter(({ l }) => l.parent_id === parent)
-			.flatMap(({ l, index }) => [
-				{ id: l.id, index, depth: parents.length, parents, hidden: l.hidden || hidden },
-				...walkLayer(doc, l.id, [l.id, ...parents], l.hidden || hidden).map((x, i, a) => ({
+			.flatMap(({ l, index }) => {
+				const children = walkLayer(doc, l.id, [l.id, ...parents], l.hidden || hidden).map((x, i, a) => ({
 					...x,
 					isLast: i + 1 === a.length
 				}))
-			]);
+
+				const own_bounding = L.get([L.cond(
+									[R.prop("box"), ['box', L.pick({
+										minX: 'position_x',
+										minY: 'position_y',
+										maxX: [L.props('position_x', 'width'), L.foldTraversalLens(L.sum, L.values)],
+										maxY: [L.props('position_y', 'height'), L.foldTraversalLens(L.sum, L.values)],
+									})]],
+									[R.prop('text'), ['text', L.pick({
+										minX: 'position_x',
+										minY: 'position_y',
+										maxX: 'position_x',
+										maxY: 'position_y',
+									})]],
+									[R.prop("edge"), ['edge', L.pick({
+										minX: L.foldTraversalLens(L.minimum, L.branch({
+										source_x: L.identity,
+										target_x: L.identity,
+										waypoints: [L.elems, 'x']
+									})),
+										minY: L.foldTraversalLens(L.minimum, L.branch({
+										source_y: L.identity,
+										target_y: L.identity,
+										waypoints: [L.elems, 'y']
+									})),
+										maxX: L.foldTraversalLens(L.maximum, L.branch({
+										source_x: L.identity,
+										target_x: L.identity,
+										waypoints: [L.elems, 'x']
+									})),
+										maxY: L.foldTraversalLens(L.maximum, L.branch({
+										source_y: L.identity,
+										target_y: L.identity,
+										waypoints: [L.elems, 'y']
+									})),
+									})]],
+								)], l)
+
+				const deep_bounding = children.reduce(({minX: AccminX,
+					minY: AccminY,
+					maxX: AccmaxX,
+					maxY: AccmaxY}, {deep_bounding: {minX,
+					minY,
+					maxX,
+					maxY}}) => ({
+						minX: Math.min(minX, AccminX),
+						minY: Math.min(minY, AccminY),
+						maxX: Math.max(maxX, AccmaxX),
+						maxY: Math.max(maxY, AccmaxY),
+					}), own_bounding)
+
+				return [
+					{ id: l.id, index, depth: parents.length, parents, hidden: l.hidden || hidden, has_children: children.length > 0, own_bounding, deep_bounding},
+					...children
+				]
+			});
 	}
 
 	function walkDocument(doc) {
@@ -844,6 +898,21 @@
 											<Grid {rotationTransform} {frameBoxObject} {cameraScale} {gridDistance} />
 										{/if}
 
+										<g transform={rotationTransform.value} opacity="0.7">
+											{#each selectedLayers.value as id (id)}
+												{@const el = view(['layers', 'items', L.find((el) => el.id == id)], doc)}
+
+												{@const deep_bounding = view([L.find((el) => el.id == id && el.has_children), 'deep_bounding'], layersInOrder).value}
+
+												{#if (deep_bounding)}
+												<rect stroke="#0af" cursor="move" stroke-dasharray="{cameraScale.value * 2} {cameraScale.value * 2}" stroke-width={cameraScale.value * 2} x={deep_bounding.minX} y={deep_bounding.minY} width={deep_bounding.maxX - deep_bounding.minX} height={deep_bounding.maxY - deep_bounding.minY} fill="#0af1"
+												onclick={evt => {
+													evt.stopPropagation()
+												}}/>
+												{/if}
+												{/each}
+											</g>
+
 										<g transform={rotationTransform.value}>
 											<g id="full-document-{data.document.id}">
 												{#each layersInOrder.value as { index, id, depth, hidden } (id)}
@@ -852,6 +921,7 @@
 															['layers', 'items', L.find((el) => el.id == id)],
 															doc
 														)}
+
 
 														{#if el.value?.box}
 															<g
@@ -1017,6 +1087,9 @@
 										<g transform={rotationTransform.value} opacity="0.7">
 											{#each selectedLayers.value as id (id)}
 												{@const el = view(['layers', 'items', L.find((el) => el.id == id)], doc)}
+
+
+
 												{#if el.value?.box}
 													<rect
 														class="selected"
