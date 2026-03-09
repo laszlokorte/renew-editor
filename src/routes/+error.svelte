654011338cf049c2661ccf781e6a9c2db5f8a881
Laszlo Korte <me@laszlokorte.de>
2026-03-09 16:54:42 +0100

    improve auth error flow

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,7 @@
 	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import AppBar from './AppBar.svelte';
+	import Error from './Error.svelte';
 
 	const { data } = $props();
 </script>
@@ -9,15 +10,7 @@
 <AppBar title={`Error ${$page.status}: ${$page.error.message}`} authState={data.authState} />
 
 <section>
-	<h1 class="center">Error {$page.status}: {$page.error.message}</h1>
-
-	{#if $page.status === 401 || $page.status === 403}
-		<p class="center">Try to <a href="{base}/auth">log out</a> and log in again.</p>
-	{/if}
-
-	<p class="center">
-		<a href="{base}/">Back to Homepage</a>
-	</p>
+	<Error {page} />
 </section>
 
 <style>
